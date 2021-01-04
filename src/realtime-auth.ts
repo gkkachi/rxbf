be709fd30088faca5a8a93aeef27d013fe142fec
Konosuke Kachi <github@s.k-kachi.net>
2021-01-04 21:58:21 +0900

    Fix bugs related to auth

--- src/realtime-auth.ts
+++ src/realtime-auth.ts
@@ -1,6 +1,8 @@
 import { createHmac, randomBytes } from 'crypto';
-import { Observable, from, concat } from 'rxjs';
-import { ignoreElements, mergeAll, shareReplay } from 'rxjs/operators';
+import { Observable, from } from 'rxjs';
+import {
+  switchMap, mergeAll, shareReplay,
+} from 'rxjs/operators';
 import RealtimeClient from './realtime';
 import type * as Types from './types';
 
@@ -12,19 +14,19 @@ export default class RealtimeAuthClient extends RealtimeClient {
       throw new Error('API key and/or secret are required.');
     }
     super();
-    this.auth$ = from(this.auth(key, secret)).pipe(shareReplay(), ignoreElements());
+    this.auth$ = from(this.auth(key, secret)).pipe(shareReplay());
   }
 
   public childOrderEvents() {
-    return this.subscribe<Types.ChildOrderEvent[]>('child_order_events').pipe(mergeAll());
+    return this.subscribeAuth<Types.ChildOrderEvent[]>('child_order_events').pipe(mergeAll());
   }
 
   public parentOrderEvents() {
-    return this.subscribe<Types.ParentOrderEvent[]>('parent_order_events').pipe(mergeAll());
+    return this.subscribeAuth<Types.ParentOrderEvent[]>('parent_order_events').pipe(mergeAll());
   }
 
-  protected subscribe<T>(channel: string) {
-    return concat(this.auth$, super.subscribe<T>(channel)) as Observable<T>;
+  protected subscribeAuth<T>(channel: string) {
+    return this.auth$.pipe(switchMap(() => this.subscribe<T>(channel)));
   }
 
   protected auth(key: string, secret: string) {
